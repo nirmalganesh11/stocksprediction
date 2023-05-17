62e581c4833bb6ecaa1801602644a9c5e1ecb1b3
Nirmal Ganesh Yarramaneni <nirmalganesh11@gmail.com>
2023-05-17 10:55:48 +0530

    stocks

---
+++ StockPricePrediction/StockPricePredictionApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    # path('',views.home,name='home'),
+    # path('compare/',views.compare,name='compare'),
+    path('download/<id>',views.download,name='download'),
+    path('predict/',views.predict,name='predict'),
+    # path('all_stocks/',views.all_stocks,name='all_stocks'),
+    path('details/<id>',views.details,name='details'),
+    
+]
